380f9e448e08df3cbc2c712824944747da870c09
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2022-02-11 10:57:28 +0100

    init

---
+++ WebApplication1/WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,44 @@
+﻿using WebApplication1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+
+        private List<bookapi> Listabooks;
+
+        public ApiController()
+        {
+            Listabooks = new List<bookapi>()
+            {
+                new bookapi() {id = 1, Title = "AAAA", Author = "BBBB", Price = "100"},
+                new bookapi() {id = 2, Title = "CCCC", Author = "DDDD", Price = "50"},
+                new bookapi() {id = 3, Title = "EEEE", Author = "FFFF", Price = "10"}
+            };
+        }
+
+        [HttpGet]
+        [Route("api/books")]
+        public ActionResult<IEnumerable<bookapi>> GetCard()
+        {
+            return Listabooks.ToList();
+        }
+        [HttpDelete("{id}")]
+        [Route("api/book/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var res = Listabooks.Find(x => x.id == id);
+            Listabooks.Remove(res);
+
+            return NoContent();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/bookapi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class bookapi
+    {
+        public int id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [RegularExpression(@"^\d+(,\d{1,2})?$")]
+        public string Price { get; set; }
+    }
+}
